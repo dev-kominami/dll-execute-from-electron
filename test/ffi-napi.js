34c536020bf7a7c7cf652cffed5933b5c2d4c4ba
dev-kominami <dev.kominami@gmail.com>
2022-08-30 18:05:44 +0900

    test: register_callback execute

--- test/ffi-napi.js
+++ test/ffi-napi.js
@@ -1,27 +1,40 @@
+/**
+ * サンプルドキュメント
+ * https://github.com/node-ffi/node-ffi/wiki/Node-FFI-Tutorial
+ */
 const ffi = require("ffi-napi");
 console.log('dll test start');
 
-const function_ptr_1 = ffi.Function("void",[]);
+// const function_ptr_1 = ffi.Function("void",[]);
+// const function_ptr_2 = ffi.Function("void",["string"]);
+
+// ddlファイルパス
 const library_file = "dll/Dll1.dll";
 
 
 // DLL側を呼び出すfunctionの定義
 const dll = ffi.Library(library_file, {
-  // register_callback: ["string", ["string", function_ptr_1]],
-  test_print: ["string", ["string", function_ptr_1]],
+  register_callback: ["void", ["pointer"]], //callbackの場合の定義
+  test_print: ["string", ["string"]],
 });
 
-// DLLを実行
+/**
+ * DLLを実行
+ * ffi.Callbackでネイティブ・ライブラリに渡すことができるポインタを返す。
+ *  */
+const callback = ffi.Callback("void", ["string"], function(callback) {
+  console.log("callback value:", callback)
+});
 
-dll.test_print("test print!!", function() {});
+console.log('register_callback start');
+dll.register_callback(callback); //callbackポインタを引数に指定
+dll.test_print("test print!!");
+console.log('done');
 
-// dll.register_callback("TEST", function(callbackVal) {
-//   console.log('DLL側のregister_callbackの実行');
-//   console.log("callbackVal=", callbackVal);
-// });
-
-
-
-// retはregister_callback自体の戻り値
-console.log('retはregister_callback自体の戻り値');
-// console.log(ret());
+/**
+ * GCを避けるため、コールバックポインタへの参照を追加する。
+ * https://github.com/node-ffi/node-ffi/wiki/Node-FFI-Tutorial#callbacks
+ **/
+process.on('exit', function() {
+  callback
+});
